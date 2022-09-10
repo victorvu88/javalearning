59484c9f6327e4cdd7f4dde2f9f1e1cbfacd4f64
NguyenChiBao3101 <baonguyen310115@gmail.com>
2022-09-10 17:55:21 +0700

    change message hello world

--- src/bao/learning/unit1/HelloWorld.java
+++ src/bao/learning/unit1/HelloWorld.java
@@ -2,6 +2,6 @@ package bao.learning.unit1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, My name is Bao");
     }
 }
